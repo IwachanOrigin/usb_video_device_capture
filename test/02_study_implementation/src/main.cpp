4a1449a2acb8636c5fa682569c053e316cb0299d
Yuji Iwanaga <iwanaga@azlab.co.jp>
2022-11-28 12:49:01 +0900

    add stdafx, helper, main

---
+++ test/02_study_implementation/src/main.cpp
@@ -0,0 +1,63 @@
+
+#include "stdafx.h"
+#include "dxhelper.h"
+
+#pragma comment(lib, "mfplat.lib")
+#pragma comment(lib, "Mf.lib")
+
+using namespace helper;
+
+void usage()
+{
+  std::cout << "" << std::endl;
+}
+
+void getVideoDeviceNames(std::vector<std::wstring>& deviceNames)
+{
+  std::shared_ptr<IMFAttributes> pAttributes;
+  IMFAttributes* pRawAttributes = nullptr;
+  ThrowIfFailed(MFCreateAttributes(&pRawAttributes, 1));
+  pAttributes = std::shared_ptr<IMFAttributes>(pRawAttributes, [](auto* p)
+  { p->Release(); });
+
+  ThrowIfFailed(pAttributes->SetGUID(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE, MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID));
+
+  IMFActivate** ppRawDevice = nullptr;
+  uint32_t count = 0;
+  ThrowIfFailed(MFEnumDeviceSources(pAttributes.get(), &ppRawDevice, &count));
+
+  deviceNames.resize(count);
+  for (int i = 0; i < count; i++)
+  {
+    wchar_t* buffer = nullptr;
+    uint32_t length = 0;
+    ThrowIfFailed(ppRawDevice[i]->GetAllocatedString(MF_DEVSOURCE_ATTRIBUTE_FRIENDLY_NAME, &buffer, &length));
+    std::wcout << "No. " << i << " : " << buffer << std::endl;
+    deviceNames[i] = buffer;
+  }
+
+  if (ppRawDevice != nullptr)
+  {
+    for (int i = 0; i < count; i++)
+    {
+      ppRawDevice[i]->Release();
+    }
+    CoTaskMemFree(ppRawDevice);
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  // INIT
+  ThrowIfFailed(CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE));
+  ThrowIfFailed(MFStartup(MF_VERSION));
+
+  std::vector<std::wstring> deviceNames;
+  getVideoDeviceNames(deviceNames);
+
+  // Release
+  ThrowIfFailed(MFShutdown());
+  CoUninitialize();
+
+  return 0;
+}
