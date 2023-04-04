e314d7be9bdbc0bdef041960afc0ff2ac455c9e2
Yuji Iwanaga <iwanaga@azlab.co.jp>
2023-04-04 12:52:43 +0900

    test/07, add devices info

---
+++ test/07_cap2Qimages/main/devicesinfo.h
@@ -0,0 +1,90 @@
+
+#ifndef DEVICES_INFO_H_
+#define DEVICES_INFO_H_
+
+#include <vector>
+#include <string>
+
+#include <mfapi.h>
+
+class DevicesInfo
+{
+  struct DeviceInfo
+  {
+    std::wstring deviceName;
+    std::wstring symbolicLink;
+  };
+
+  struct DeviceMediaInfo
+  {
+    uint32_t width;
+    uint32_t height;
+    uint32_t interlaceMode;
+    LONG stride;
+    uint32_t aspectRatioNumerator;
+    uint32_t aspectRatioDenominator;
+    uint32_t frameRateNumerator;
+    uint32_t frameRateDenominator;
+    GUID formatSubtypeGuid;
+    std::wstring formatSubtypeName;
+
+    // for std::unique
+    bool operator==(const DeviceMediaInfo& dmi)
+    {
+      if (dmi.width == this->width
+          && dmi.height == this->height
+          && dmi.interlaceMode == this->interlaceMode
+          && dmi.stride == this->stride
+          && dmi.aspectRatioNumerator == this->aspectRatioNumerator
+          && dmi.aspectRatioDenominator == this->aspectRatioDenominator
+          && dmi.frameRateNumerator == this->frameRateNumerator
+          && dmi.frameRateDenominator == this->frameRateDenominator
+          && dmi.formatSubtypeGuid == this->formatSubtypeGuid)
+      {
+        return true;
+      }
+      return false;
+    }
+
+    // for std::sort
+    bool operator<(const DeviceMediaInfo& dmi) const
+    {
+      if (this->formatSubtypeName < dmi.formatSubtypeName)
+      {
+        return true;
+      }
+      return false;
+    }
+  };
+
+public:
+  explicit DevicesInfo();
+  ~DevicesInfo();
+
+  void setCurrentVideoDeviceIndex(int index)
+  { 
+    m_currentVideoDeviceIndex = index;
+    this->getVideoDeviceMediaInfo();
+  }
+  void setCurrentAudioDeviceIndex(int index)
+  {
+    m_currentAudioDeviceIndex = index;
+    this->getAudioDeviceMediaInfo();
+  }
+
+  void writeDeviceNameList();
+  void writeDeviceMediaInfoList();
+
+private:
+  int getDeviceNames();
+  int getVideoDeviceMediaInfo();
+  int getAudioDeviceMediaInfo();
+
+  std::vector<DeviceInfo> m_devicesInfo;
+  std::vector<DeviceMediaInfo> m_deviceMediaInfo;
+  int m_currentVideoDeviceIndex;
+  int m_currentAudioDeviceIndex;
+
+};
+
+#endif // DEVICES_INFO_H_
--- test/07_cap2Qimages/main/main.cpp
+++ test/07_cap2Qimages/main/main.cpp
@@ -2,6 +2,11 @@
 #include "mainwindow.h"
 #include <QtWidgets/QApplication>
 
+#pragma comment(lib, "mfplat.lib")
+#pragma comment(lib, "mf.lib")
+#pragma comment(lib, "mfreadwrite.lib")
+#pragma comment(lib, "mfuuid.lib")
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
